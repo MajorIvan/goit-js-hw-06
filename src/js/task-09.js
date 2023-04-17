15d6f741c0cfa7b87f58f014b0dc314ce0e59b03
Ivan Moshak <118841894+MajorIvan@users.noreply.github.com>
2023-04-17 15:27:46 +0300

    Update

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector('.widget');
+const colorSpan = widget.querySelector('.color');
+const changeColorBtn = widget.querySelector('.change-color');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
