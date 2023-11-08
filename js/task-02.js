c0650d4ac3b61fe56f0be7d226822806ddd1be3f
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-08 14:11:31 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,3 +7,20 @@ const ingredients = [
   'Condiments',
 ];
 
+const ul = document.querySelector('#ingredients');
+
+function createMarkup(arr) {
+  
+  const markup = arr.map ( item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+
+    li.classList.add('item');
+
+    return li;
+  });
+
+  ul.append(...markup);
+}
+
+createMarkup(ingredients);
