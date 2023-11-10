3815eb66807c406efe50a0bf3150ea905e0f159f
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-10 10:51:26 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', changeColor);
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor; 
+  color.textContent = randomColor;
+}
