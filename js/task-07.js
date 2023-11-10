5d92eb50235fb6682a37072cb3e467fabf451aed
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-10 10:11:00 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,10 @@
 const fontSizeControl = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
+text.style.fontSize = `56px`;
 
 fontSizeControl.addEventListener('input', handleInput);
 
 function handleInput(event) {
-  const fontSize = event.currentTarget.value + 'px';
+  const fontSize = `${event.currentTarget.value}px`;
   text.style.fontSize = fontSize;
 }
