f3ff5d4cf3ac235a775cd97aa4385bf9242a8b57
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-10 10:38:39 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+
+    if (email.value === '' || password.value === '') {
+        alert ('All fields must be filled');
+        return;
+    };
+
+    console.log('Email:', email.value);
+    console.log('Password:', password.value);
+
+    form.reset();
+}
